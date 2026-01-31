34134bf9eb4cd9b01f72678482ce3e86aeb68b1c
VLauren <laurentino.vilchez@gmail.com>
2026-01-31 20:12:34 +0100

    Menus stuff

---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ShowMission());
+    }
+
+    IEnumerator ShowMission()
+    {
+        transform.Find("Mission").gameObject.SetActive(true);
+        transform.Find("DialogueBox").gameObject.SetActive(false);
+        
+        yield return new WaitForSeconds(4);
+
+        transform.Find("Mission").gameObject.SetActive(false);
+    }
+
+    IEnumerator ShowTransition()
+    {
+        transform.Find("TransitionUI").gameObject.SetActive(true);
+
+        transform.Find("Mission").gameObject.SetActive(false);
+        transform.Find("DialogueBox").gameObject.SetActive(false);
+
+        yield return null;
+    }
+}
--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void GoToIntro()
     {
-        
+        transform.Find("MainMenu").gameObject.SetActive(false);
+        transform.Find("Intro").gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 }
