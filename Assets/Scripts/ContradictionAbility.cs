81a048fabad7da662869cfeef04e0349ac9221f0
Maikel-Ortega <maikel.or.hi@gmail.com>
2026-01-31 17:51:50 +0100

    contradictionAbility

---
+++ Assets/Scripts/ContradictionAbility.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.PackageManager;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class ContradictionAbility : MonoBehaviour
+{
+    public Transform rayOrigin;
+
+    public LayerMask contradictionMarkerMask;
+    public LayerMask deployablePlacesMask;
+
+    public GameObject contradictionMarkerPrefab;
+    
+    public List<GameObject> deployedMarkers = new List<GameObject>();
+
+    private void Start()
+    {
+        deployedMarkers = new List<GameObject>();
+    }
+
+    public void Execute()
+    {
+        //Launch raycast
+        RaycastHit hitInfo;
+        Color debugColor = Color.green;
+        if(Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hitInfo, 9999,contradictionMarkerMask))
+        {
+            //If hits ContradictionMarker, remove it. 
+            DeleteMarker(hitInfo.collider.gameObject);
+        }        
+        else
+        {
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hitInfo, 9999, deployablePlacesMask))
+            {
+                //If not, if hits good surface, deploy one. 
+                DeployMarker(hitInfo.point, hitInfo.normal);
+                debugColor = Color.blue;
+            }
+            else
+            {
+                debugColor = Color.red;
+            }
+        }
+        Debug.DrawRay(rayOrigin.position, rayOrigin.forward, debugColor, 1f);
+
+    }
+
+    public void DeployMarker(Vector3 pos, Vector3 normal)
+    {
+        GameObject go = Instantiate(contradictionMarkerPrefab,pos, Quaternion.identity);
+        deployedMarkers.Add(go);
+    }
+
+    public void DeleteMarker(GameObject go)
+    {
+        deployedMarkers.Remove(go);
+        Destroy(go);
+    }
+}
