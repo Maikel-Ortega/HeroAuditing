5daf2be284a44893af7ff88cc68384515172f27a
VLauren <laurentino.vilchez@gmail.com>
2026-02-01 12:02:37 +0100

    main menu pretty

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,13 +1,20 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public string introEs;
+    public string introEn;
+
     public void GoToIntro()
     {
         transform.Find("MainMenu").gameObject.SetActive(false);
         transform.Find("Intro").gameObject.SetActive(true);
+
+
+        transform.Find("Intro/IntroText").GetComponent<TMP_Text>().text = introEn;
     }
 
     public void StartGame()
